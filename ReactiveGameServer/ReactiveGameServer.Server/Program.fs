6de130dd80b64399d17ec9a1d8b34d53b72203f1
Anthony Brown <bruinbrown@gmail.com>
2014-06-08 22:21:13 +0100

    Added StartListening function to the server and also argument parsing

--- ReactiveGameServer/ReactiveGameServer.Server/Program.fs
+++ ReactiveGameServer/ReactiveGameServer.Server/Program.fs
@@ -1,7 +1,32 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿namespace ReactiveGameServer.Server
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open Nessos.UnionArgParser
+
+type Arguments =
+    | [<Mandatory>] Port of int
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Port _ -> "Port on which to listen"
+
+module Core =
+
+    open System
+    open System.Net
+    open System.Net.Sockets
+    open ReactiveGameServer.Shared.Network
+
+    let StartListening (port:int) =
+        let client = new UdpClient(port)
+        let obs = UdpClientToObservable client
+        client, obs
+
+    [<EntryPoint>]
+    let main argv = 
+        
+        let parser = UnionArgParser<Arguments>()
+        let results = parser.Parse(argv)
+        let port : int = results.GetResult <@ Port @>
+        let client, observable = StartListening port
+        Console.ReadLine () |> ignore
+        0
---
+++ ReactiveGameServer/ReactiveGameServer.Shared/Network.fs
@@ -0,0 +1,11 @@
+﻿namespace ReactiveGameServer.Shared
+
+module Network =
+
+    open System.Net.Sockets
+    open System.Reactive
+    open System.Reactive.Linq
+    open System.Reactive.Threading.Tasks
+
+    let UdpClientToObservable (client:UdpClient) =
+        Observable.Defer(fun () -> client.ReceiveAsync().ToObservable()).Repeat()
