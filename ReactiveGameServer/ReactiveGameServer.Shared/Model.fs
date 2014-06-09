1bb7693f21f2dcc49757006dc902d385ec6999ef
Anthony Brown <bruinbrown@gmail.com>
2014-06-09 16:24:43 +0100

    Updated the model to fit a bit better

--- ReactiveGameServer/ReactiveGameServer.Shared/Model.fs
+++ ReactiveGameServer/ReactiveGameServer.Shared/Model.fs
@@ -2,12 +2,18 @@
 
 open System
 
+type Direction =
+    | Up
+    | Down
+    | Left
+    | Right
+
 // Extend this if your users can change key mappings so it could end up being
 // | Move of Direction
 // | Shoot of Target
 // | Jump
 type UnitCommand =
-    | KeyPress of char
+    | Movement of Direction
 
 // You may want more messages here if you have more client options
 // These are the messages that a client can send to the server
@@ -22,10 +28,23 @@ type Client = {
     Position : (int * int)
 }
 
+type PlayerInfo = {
+    Id : Guid
+    Position : (int * int)
+    Username : string
+}
+
+// We need to handle the case in which a player gets moved from our original position,
+// We'll maintain a list of Player on the client in which we have a single local player and multiple remote players
+// Every time we receive a server update we fold down our (int * int) list and then apply that to our receivved position
+type Player =
+    | Local of Client * (int * int) list
+    | Remote of Client
+
 // Extend this if you want to support things like kicking players for inactivity etc
 // These are the messages a server can send to the client
 type ServerMessage =
-    | ConnectionConfirmation of Guid * (int * int)
+    | ConnectionConfirmation of Guid * (int * int) * PlayerInfo list
     | WorldUpdate of Client list
-    | ClientJoin of string * Guid
+    | ClientJoin of string * Guid * (int * int)
     | ClientLeft of Guid
